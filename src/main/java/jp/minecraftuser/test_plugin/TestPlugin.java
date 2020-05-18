c8006dc223f2544899a491052b1c4fd35ba7effe
katot213 <katot213@kklab.spub.chitose.ac.jp>
2020-05-18 23:52:16 +0900

    リファクタリング

--- src/main/java/jp/minecraftuser/test_plugin/TestPlugin.java
+++ src/main/java/jp/minecraftuser/test_plugin/TestPlugin.java
@@ -11,7 +11,7 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TestPlugin extends JavaPlugin implements Listener {
+public class TestPlugin extends JavaPlugin {
     /**
      * 起動時処理
      */
@@ -19,7 +19,7 @@ public class TestPlugin extends JavaPlugin implements Listener {
     public void onEnable() {
         getLogger().info("test enable: OMMC");
         // イベントリスナーの登録
-        getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(new KanbanListener(this), this);
     }
 
     /**
@@ -34,39 +34,5 @@ public class TestPlugin extends JavaPlugin implements Listener {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         sender.sendMessage("Command, Test Plugin!");
         return true;
-    }
-
-    /**
-     * プレイヤーがクリックした時に呼び出されるメソッド
-     * @param event
-     */
-    @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        getLogger().info("execute!!!");
-        // ブロック左クリックでもブロック右クリックでもなければ無視する
-        if ( event.getAction() != Action.RIGHT_CLICK_BLOCK &&
-                event.getAction() != Action.LEFT_CLICK_BLOCK ) {
-        	getLogger().info("" + event.getAction());
-            return;
-        }
-
-        // クリック先が看板でなければ無視する
-        Block block = event.getClickedBlock();
-        if ( block == null) {
-        	getLogger().info("block == null");
-        	return;
-        }
-
-        if (block.getType() != Material.ACACIA_SIGN) {
-        	getLogger().info("block.getType(): " + block.getType());
-            return;
-        }
-
-        getLogger().info("block.getState");
-        Sign sign = (Sign)block.getState();
-        getLogger().info("sign: " + sign);
-        sign.setLine(0, "hogehoge");
-        sign.setLine(1, "piyopiyo");
-        sign.update();
     }
 }
